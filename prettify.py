3d919d07c353f1bacc3e668edb004ee138f6ba9b
josh_wuz_here <joshuadcchung@gmail.com>
2024-07-05 23:26:34 +0800

    First Commit

---
+++ prettify.py
@@ -0,0 +1,36 @@
+def prettify(json_text):
+    tabs = [0]
+    text = ''
+    index = 0
+    while index < len(json_text):
+        if index < len(json_text)-1:
+            if (json_text[index] == '{' or json_text[index] == '[') and json_text[index-1] != "'":
+                text += f'{json_text[index]}£'
+                tabs.append(tabs[-1]+1)
+            elif json_text[index] == ',':
+                text += ',£'
+                index += 1
+                tabs.append(tabs[-1])
+            elif (json_text[index] == '}' or json_text[index] == ']') and json_text[index+1] != "'":
+                if json_text[index+1] == ',':
+                    text += f'£{json_text[index]},£'
+                    tabs.append(tabs[-1]-1)
+                    tabs.append(tabs[-1])
+                    index += 2
+                else:
+                    text += f'£{json_text[index]}'
+                    tabs.append(tabs[-1]-1)
+            else:
+                text += f'{json_text[index]}'
+        index += 1
+
+    new = text.split('£')
+    for index, word in enumerate(new):
+        print("    "*tabs[index]+word)
+    print('}')
+
+mytext = open("json_text.txt")
+prettify(mytext.readline())
+mytext.close()
+
+
